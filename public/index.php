087b2b17eba04fc304e8c48a146732799dca6901
amilashanaka <dsa.amilashanaka@gmail.com>
2023-03-12 03:48:56 +0000

    update

--- public/index.php
+++ public/index.php
@@ -1,5 +1,24 @@
 <?php
-require_once('../app/init.php');
 
-$app = new App();
+require_once __DIR__ . './../vendor/autoload.php';
 
+
+use app\core\Application;
+
+$app= new Application();
+// $router= new Router();
+$app->router->get('/', function(){
+
+    return 'Hello World';
+
+});
+
+$app->router->get('/contact', function(){
+
+    return 'Contact';
+
+});
+
+// $app->userRouter($router);
+
+$app->run();
